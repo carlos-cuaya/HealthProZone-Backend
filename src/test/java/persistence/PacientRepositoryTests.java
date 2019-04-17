29e95760017b2ba4d9b0817a5cee398b9d4ee124
cacuaya <cacuaya@consisint.com>
2019-04-17 16:47:55 -0500

    more asserts

--- src/test/java/persistence/PacientRepositoryTests.java
+++ src/test/java/persistence/PacientRepositoryTests.java
@@ -6,6 +6,8 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.*;
+
 import app.HealthProZoneBackendApplication;
 
 @RunWith(SpringRunner.class)
@@ -17,6 +19,13 @@ public class PacientRepositoryTests {
 
 	@Test
 	public void getAllPatientsTest(){
-		patientRepository.findAll();
+		assertThat(patientRepository.findAll()).isEmpty();
+
+		assertThat(patientRepository.findAll()).hasSize(1);
+        //assertThat(findAll).containsOnly(webBooking);
+        //assertThat(customer.getId()).isGreaterThan(-1L);
+		//assertThat(customer.getName()).isEqualTo("T. Testing");
+		//assertThat(customer.getEmail()).isEqualTo("t.testing@test123.tst");
+		//assertThat(repository.findById(customer.getId())).isEqualTo(customer);
 	}
 }
